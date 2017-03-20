0e04190210341bc8767b24a5365034b40d00adfb
Seonho Kim <ksseono@gmail.com>
2017-03-20 16:28:23 +0900

    update dependencies versions

--- project.clj
+++ project.clj
@@ -4,22 +4,23 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure         "1.6.0"]
-                 [com.stuartsierra/component  "0.2.3"]
-                 [org.clojure/tools.namespace "0.2.10"]
-                 [org.clojure/tools.cli       "0.3.1"]
-                 [compojure                   "1.3.4"]
-                 [ring/ring-jetty-adapter     "1.3.2"]
-                 [ring/ring-defaults          "0.1.5"]
+  :dependencies [[org.clojure/clojure         "1.8.0"]
+                 [com.stuartsierra/component  "0.3.2"]
+                 [org.clojure/tools.namespace "0.2.11"]
+                 [org.clojure/tools.cli       "0.3.5"]
+                 [compojure                   "1.5.2"]
+                 [ring/ring-jetty-adapter     "1.5.1"]
+                 [ring/ring-defaults          "0.2.3"]
                  [org.clojure/data.json       "0.2.6"]
-                 [com.taoensso/carmine        "2.10.0"]
-                 [com.taoensso/timbre         "3.4.0"]
-                 [clj-http-lite               "0.2.1"]
-                 [im.chit/cronj               "1.4.3"]]
+                 [com.taoensso/carmine        "2.15.1"]
+                 [com.taoensso/timbre         "4.8.0"]
+                 [com.taoensso/encore         "2.90.1"]
+                 [clj-http-lite               "0.3.0"]
+                 [im.chit/cronj               "1.4.4"]]
 
   :profiles {:dev {:dependencies
                    [[org.clojars.runa/conjure "2.2.0"]
-                    [ring/ring-mock           "0.2.0"]]
+                    [ring/ring-mock           "0.3.0"]]
                    :source-paths ["dev"]}}
 
   :codox {:exclude [user dev]
